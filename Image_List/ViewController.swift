8b57c5f1213c357104ca09553d8d3eebc4d36597
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-05 15:40:15 +0300

    Override tableview cellForRowAt function and create cell

--- Image_List/ViewController.swift
+++ Image_List/ViewController.swift
@@ -38,6 +38,13 @@ class ViewController: UITableViewController  {
         return pictures.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
 
 }
 
