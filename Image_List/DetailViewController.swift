043d9ac707ae2338d167cbb55384e93131b8fed6
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-05 16:26:38 +0300

    Create var selectedImage

--- Image_List/DetailViewController.swift
+++ Image_List/DetailViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
